7093456c8bfa8e8432752bd53ed1a2b9e51da7d2
Mauricio Julio Rodrigo Lemus Guzmán <93178012+Rodlemus03@users.noreply.github.com>
2024-08-26 14:13:30 -0600

    Migración métodos utilities

--- src/main/java/io/github/josecarlosbran/SeleniumUtils.java
+++ src/main/java/io/github/josecarlosbran/SeleniumUtils.java
@@ -7,6 +7,10 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.testng.Assert;
 
+import java.io.File;
+import java.net.URI;
+import java.util.ArrayList;
+
 public class SeleniumUtils {
     /***
      * Método que presiona una tecla del teclado simulado por selenium
@@ -42,7 +46,47 @@ public class SeleniumUtils {
         }
     }
 
+    /**
+     * Función que convierte un array de objetos en un ArrayList de cadenas
+     *
+     * @param object El Array de objetos que se desea convertit en cadenas
+     * @param acierto Variable booleana que decide si será un acierto(True) o un fallo (False)
+     * @return Un ArrayList que contiene las representaciones en forma de cadena de los objetos
+     */
+    public static ArrayList<String> convertObjectToArrayString(Object[] object, Boolean acierto) {
+        if(acierto) {
 
+
+            try {
+                ArrayList<String> array = new ArrayList<>();
+                for (Object o : object) {
+                    array.add((o.toString()));
+                }
+                return array;
+            } catch (Exception e) {
+                LogsJB.fatal("error al parsear el Objeto object a strings");
+                LogsJB.fatal(e.getMessage());
+                LogsJB.fatal("Stacktrace de la excepción: " + ExceptionUtils.getStackTrace(e));
+            }
+        }else{
+            LogsJB.fatal("error al parsear el Objeto object a strings");
+        }
+        return null;
+
+    }
+
+    /***
+     * Hace una pausa sobre el hilo en ejecución por el tiempo especificado
+     * @param milisegundos Tiempo en milisegundos que se detendra la ejecucion
+     */
+    public static void threadslepp(int milisegundos) {
+        try {
+            Thread.sleep(milisegundos);
+        } catch (Exception e) {
+            LogsJB.fatal("Se ha capturado un error en threadsleep");
+            LogsJB.fatal("Stacktrace de la excepcion capturada: " + ExceptionUtils.getStackTrace(e));
+        }
+    }
 
 
 
